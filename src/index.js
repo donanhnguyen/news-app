5b258253b9774deb9906e31363e23586a1b3fb01
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-03-18 11:25:14 -0500

    trying again

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path='/' element={ <App/> }></Route>
+        <Route exact path='/' element={ <App/> }></Route>
         <Route path='/displayNews/:countryCode' element={ <DisplayNews/> }></Route>
         <Route path='/displayNews/:countryCode/:storyId' element={ <DisplayNewsStory/> }></Route>
       </Routes>
